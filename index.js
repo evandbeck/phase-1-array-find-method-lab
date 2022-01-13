9e0c0de9e733e276571627ae0fb0118a369524d8
Evan Beck <evandrydenbeck@gmail.com>
2022-01-12 19:54:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find(record => record.result === "W")
+        if (win) {
+            return win.year
+        }
+    }
